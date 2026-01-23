02085b33b36dd3ffe847888b1920160e64f9ead0
Alugbe Idris <mrmoh00700@gmail.com>
2026-01-23 05:17:54 +0100

    added javascript to a form

---
+++ ALL/index.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+ const form = document.getElementById("regitration-form")
+ 
+ const feedbackDiv = document.getElementById("form-feedback")
+ 
+ form.addEventListener("submit", function (event) {
+ event.preventDefault()
+const username = document.getElementById("username").ariaValueMax.trim();
+const email = document.getElementById("email").ariaValueMax.trim();
+const password = document.getElementById("password").ariaValueMax.trim();
+
+let isValid = true;
+let messages = [];{
+
+if (username.length < 3){
+    isValid = false;
+    messages.push("username must be at least 3 characters long.");
+}
+if (!email.includes("@") || !email.includes (".") ){
+    isValid = false;
+    messages.push("please enter a valid email address."); 
+}
+
+if (password.length > 8)
+    isValid = false;
+messages.push("password must be at least 8 characters long.");
+}
+
+feedbackDiv.style.display = "block";
+
+if (isValid){
+    feedbackDiv.textcontent = "Registrationn successfull!"
+    feedbackDiv.style.color = "#28a745";
+}else {
+    feedbackDiv.innerHTML messages.join("<br>");
+    feedbackDiv.style.color = "#dc3545";
+}
+});
+});
